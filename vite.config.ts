5a4c15ce8c6d7c3d76e31b0278bac234850e8a24
franciumm <111029280+franciumm@users.noreply.github.com>
2026-01-08 19:24:30 +0200

    setting up phone performance Error stop

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import path from 'path';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), 
+  plugins: [
+    react(),
     ViteImageOptimizer({
       png: { quality: 80 },
       jpeg: { quality: 80 },
-      webp: {  quality: 80 },
+      webp: { quality: 80 },
       svg: { multipass: true },
-    }),],
+    }),
+  ],
   resolve: {
     alias: {
-      // 👇 This line fixes the "Failed to resolve import" errors
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    chunkSizeWarningLimit: 1000, 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
-})
+});
