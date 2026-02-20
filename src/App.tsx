ae4de49318758832a394147a653a07a3a67ff812
franciumm <111029280+franciumm@users.noreply.github.com>
2026-02-20 13:21:27 +0200

    MasterTimeline Adding in the scheduler prompt

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
 import StarBackground from "./components/StarBackground";
 import { SpeedInsights } from '@vercel/speed-insights/react';
-
+import { Analytics } from "@vercel/analytics/next"
 // ⚡️ LAZY LOAD THE PAGES: This prevents the heavy JS from blocking the initial load
 const Index = lazy(() => import("./pages/Index")); 
 const NotFound = lazy(() => import("./pages/NotFound"));
@@ -20,6 +20,7 @@ const App = () => (
     <HelmetProviderFixed> 
       <TooltipProvider>
         <Toaster />
+        <Analytics/>
         <Sonner />
         
         {/* Background is always visible */}
