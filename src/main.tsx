0926d84342c4c95dcfc2fb07a2f1be2d16164207
franciumm <111029280+franciumm@users.noreply.github.com>
2026-02-21 19:34:59 +0200

    HI

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import posthog from 'posthog-js'
 import { HelmetProvider } from 'react-helmet-async'
-
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  autocapture: true,
-  disable_session_recording: true, // Started later
-  loaded: (ph) => {
-    // ⚡️ Wait for the browser to be "Idle" before starting heavy recording
-    if ('requestIdleCallback' in window) {
-      window.requestIdleCallback(() => {
-        setTimeout(() => ph.startSessionRecording(), 5000);
-      });
-    } else {
-      setTimeout(() => ph.startSessionRecording(), 6000);
-    }
-  }
-})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
