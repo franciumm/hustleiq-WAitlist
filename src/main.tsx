e258dea3e92ca82f1b4af58fc603d8649c6d6e2c
franciumm <111029280+franciumm@users.noreply.github.com>
2026-02-24 20:40:49 +0200

    Performance fixing

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async'
-
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/700.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
